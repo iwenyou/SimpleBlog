f9f986823d2fb241fb80baa26eaa8f9a781a24a4
Wen Zhang <iwenyou@gmail.com>
2017-08-25 23:33:20 -0700

    added PostsNew

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+render PostsNew extends Component {
+  render() {
+    return(
+      <div>
+        postnew!
+      </div>
+    );
+  };
+}
+
+export default PostsNew;
