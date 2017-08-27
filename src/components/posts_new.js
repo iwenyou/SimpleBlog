6aec36e2021567895f43b8cb10c3e63aea812982
Wen Zhang <iwenyou@gmail.com>
2017-08-26 23:21:50 -0700

    added redux-form to posts-new

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return(
       <div>
-        
+        <form>
+          <Field
+            name="title"
+            component={}
+            />
+        </form>
       </div>
     );
   };
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
