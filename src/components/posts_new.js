bff75b63e16b6c684ba1eb04805c0e0a76648e33
Wen Zhang <iwenyou@gmail.com>
2017-08-27 00:14:03 -0700

    added name field in posts_new

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderTitleField(field){
+    return (
+      <div className="form-group">
+        <label>Title:</label>
+        <input
+          type="text"
+          className="form-control"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return(
       <div>
         <form>
           <Field
             name="title"
-            component={}
-            />
+            component={this.renderTitleField}
+          />
+        <Field
+          name="tags"
+          component={this.renderTagsField}
+          />
         </form>
       </div>
     );
