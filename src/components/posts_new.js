e54fa1a820099c6ceb9bb671d40229c122dfb935
Wen Zhang <iwenyou@gmail.com>
2017-08-29 11:07:13 -0700

    added axios post request, connected form to redux.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,24 +1,36 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 
   renderField(field) {
+
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger': ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}:</label>
         <input
           type="text"
           className="form-control"
           {...field.input}
         />
-      {field.meta.touched ? field.meta.error: ""}
+      <div className="text-help">
+        { touched ? error: ""}
+      </div>
       </div>
     );
   }
 
   onSubmit(values){
-    console.log(values);
+    this.props.history.push('/');
+    this.props.createPost(values, () => {
+      
+    });
   }
 
   render() {
@@ -31,6 +43,7 @@ class PostsNew extends Component {
           <Field label="Categories" name="categories" component={this.renderField}/>
           <Field label="Post Content" name="content" component={this.renderField}/>
           <button type="submit" className="btn btn-primary">Submit</button>
+          <Link to="/" className="btn btn-danger">Cancel</Link>
         </form>
       </div>
     );
@@ -61,4 +74,6 @@ function validate(value){
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null,{ createPost })(PostsNew)
+);
